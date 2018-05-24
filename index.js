23092259da4d7bf6603dec87137d2d1d53d560ed
Zheliang Tan <zheliangtan@gmail.com>
2018-05-25 00:40:04 +0800

    Implemented dynamic cmd handler

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-// require the discord.js module
+// Import discord.js module
 const Discord = require('discord.js');
 
-// create a new Discord client
-const client = new Discord.Client();
+// Import node.js file system module
+const fs = require('fs');
 
 // Import config files
 const { prefix, token } = require('./config.json');
+
+// Create a new Discord client
+const client = new Discord.Client();
+
+// Create a new discord commands collection
+client.commands = new Discord.Collection();
+
+// Dynamically retrieve command files
+const commandFiles = fs.readdirSync('./commands');
+
+// Loop to import command files from command folder
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+	console.log('Loaded command', command.name);
+}
 
 // when the client is ready, run this code
 // this event will trigger whenever your bot:
@@ -15,26 +31,21 @@ client.on('ready', () => {
 	console.log('Hello, my name is RubyBot.');
 });
 
-// simple command handler
+// Dynamic command handler
 client.on('message', (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
 	}
-	else if (command === 'server') {
-		message.channel.send(
-			`Member count: ${message.guild.memberCount}`
-		);
-	}
-	else if (command === 'cmd-info') {
-		message.channel.send(
-			`Total arguemnts: ${args.length}
-			Your arguments are: ${args}`
-		);
+	catch (error) {
+		console.log(error);
+		message.reply('there was an error in trying to execute your command.');
 	}
 });
 
